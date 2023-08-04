3bbc6565c573ffb23abdd2281c9e447128c93037
Artikaup <artikaup@gmail.com>
2023-08-04 12:18:15 -0300

    agregue el foreach en eden2

--- cypress/e2e/Eden/eden2.cy.js
+++ cypress/e2e/Eden/eden2.cy.js
@@ -15,14 +15,20 @@ describe('test pagina EDEN', () => {
     it('verificar Menu', () => {
       
         cy.visit("https://www.edenentradas.com.ar/");
-        edenHeader2.getMenuButtons().eq(0).should("contain.text","HOME");
+
+        const menuBtn = ["HOME","TODOS","AGENDA DEL FINDE","RECITALES","TEATROS","CUARTETOS","FESTIVALES","SALAS"];
+        menuBtn.forEach((txtBtn, $index)=>{
+          edenHeader2.getMenuButtons().eq($index).should("contain.text",txtBtn);
+        })
+
+        /*edenHeader2.getMenuButtons().eq(0).should("contain.text","HOME");
         edenHeader2.getMenuButtons().eq(1).should("contain.text","TODOS");
         edenHeader2.getMenuButtons().eq(2).should("contain.text","AGENDA DEL FINDE");
         edenHeader2.getMenuButtons().eq(3).should("contain.text","RECITALES");
         edenHeader2.getMenuButtons().eq(4).should("contain.text","TEATROS");
         edenHeader2.getMenuButtons().eq(5).should("contain.text","CUARTETOS");
         edenHeader2.getMenuButtons().eq(6).should("contain.text","FESTIVALES");
-        edenHeader2.getMenuButtons().eq(7).should("contain.text","SALAS");
+        edenHeader2.getMenuButtons().eq(7).should("contain.text","SALAS");*/
       });
       it('verificar pagina RECITALES', () => {
       
