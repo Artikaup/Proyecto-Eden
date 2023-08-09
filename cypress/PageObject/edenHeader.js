0183480c3c459d809857b864178392564210cbc7
Artikaup <artikaup@gmail.com>
2023-08-08 23:23:00 -0300

    Guardo los cambios en eden.cy.js

--- cypress/PageObject/edenHeader.js
+++ cypress/PageObject/edenHeader.js
@@ -2,7 +2,14 @@
 
 class EdenHeaderLocators{
     constructor(){
+        //Botonera y header principal
+        this.imageLogo = "#header-logo";
         this.menuButtons = "#navbar a";
+        
+        //Sección de busqueda
+        this.searchInput = "#espectaculoList";
+        this.searchSuggestion = ".ui-menu-item";
+
     
     }
 }
@@ -11,8 +18,18 @@ export default class EdenHeader{
     constructor(){
         this.locators = new EdenHeaderLocators;
     }
+    //Botonera y header principal
+    getImageLogo(){
+        return cy.get(this.locators.imageLogo);
+    }
     getMenuButtons(){
         return cy.get(this.locators.menuButtons);
     }
-    
+    //Sección de busqueda
+    getSearchInput(){
+        return cy.get(this.locators.searchInput);
+    }
+    getSearchSuggestion(){
+        return cy.get(this.locators.searchSuggestion);
+    }
 }
--- cypress/e2e/Eden/eden.cy.js
+++ cypress/e2e/Eden/eden.cy.js
@@ -2,13 +2,16 @@
 
 import EdenHeader from "../../PageObject/edenHeader";
 import EdenHome from "../../PageObject/edenHome";
+import EdenEvent from "../../PageObject/edenEvent";
+
 const edenHeader = new EdenHeader();
 const edenHome = new EdenHome();
+const edenEvent = new EdenEvent();
 
-describe('test pagina EDEN', () => {
+describe('Test pagina EDEN', () => {
     
   
-    it('verificar subtitulos', () => {
+    it('Verificar subtitulos', () => {
       
       cy.visit("https://www.edenentradas.com.ar/");
 
@@ -18,7 +21,7 @@ describe('test pagina EDEN', () => {
       edenHome.getSubTitles().last().should("contain.text", txtCalendar);
     });
   
-    it('verificar Menu', () => {
+    it('Verificar Menu', () => {
       
         cy.visit("https://www.edenentradas.com.ar/");
 
@@ -35,11 +38,33 @@ describe('test pagina EDEN', () => {
         edenHeader.getMenuButtons().eq(6).should("contain.text",menuBtn [5]);
         edenHeader.getMenuButtons().eq(7).should("contain.text",menuBtn [6]);*/
       });
-      it('verificar pagina RECITALES', () => {
+      it('Verificar pagina RECITALES', () => {
       
         cy.visit("https://www.edenentradas.com.ar/");
-        edenHeader.getMenuButtons().eq(3).click();
+        edenHeader.getMenuButtons().contains("RECITALES").click();
+
+        cy.url().should("include","/sitio/contenido/recitales");
+        /*const newUrl = "https://www.edenentradas.com.ar/sitio/contenido/recitales";
+        cy.url().should("eq", newUrl)
+        /*cy.url().should("eq","https://www.edenentradas.com.ar/sitio/contenido/recitales"); esta es una manera sin usar const*/
         
+      });
+
+      it('Verificar imagen de logo',() => {
+
+        cy.visit("https://www.edenentradas.com.ar/");
+        edenHeader.getImageLogo().should("be.visible").and ("have.prop", "naturalHeight").and("be.greaterThan",0);
+        const imgSource = "https://static.edenentradas.com.ar/sitio/images/logo.gif";
+        edenHeader.getImageLogo().should("have.attr","src", imgSource);
+        edenHeader.getImageLogo().should("have.attr","alt","EdenEntradas");
+      });
+
+      it.only('Verificar funcionamiento del buscador',() =>{
+        cy.visit("https://www.edenentradas.com.ar/");
+        edenHeader.getSearchInput().type("Queen");
+        edenHeader.getSearchSuggestion().click();
+        const eventTxt = 'Experiencia Queen "Champions of the World Tour 23" ';
+        edenEvent.getEventTitle().should("contain.text", eventTxt);
       });
   });
   
